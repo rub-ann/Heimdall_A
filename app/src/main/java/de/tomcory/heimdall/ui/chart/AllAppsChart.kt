518dbfdc4297b12ec2270ad541423156e719b22c
Leon Rohmann <leon.rohmann@campus.tu-berlin.de>
2023-09-22 17:25:00 +0200

    better colors and arcs in Overview Graph

--- app/src/main/java/de/tomcory/heimdall/ui/chart/AllAppsChart.kt
+++ app/src/main/java/de/tomcory/heimdall/ui/chart/AllAppsChart.kt
@@ -1,8 +1,10 @@
 package de.tomcory.heimdall.ui.chart
 
+import android.content.res.Resources
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,20 +14,19 @@ import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import de.tomcory.heimdall.ui.theme.BlueGradientBrush
-import de.tomcory.heimdall.ui.theme.GrayScaleGradientBrush
+import de.tomcory.heimdall.ui.theme.*
 import timber.log.Timber
 
 @Composable
 fun AllAppsChart(
-    thickness: Dp = 20.dp,
+    thickness: Dp = 12.dp,
     size: Dp = 240.dp,
     bottomGap: Float = 60f,
     appSets: List<Pair<Int, Color>>
 ) {
-
     val total = appSets.sumOf { it.first }
     val arcRange = 360f - bottomGap
 
@@ -38,12 +39,34 @@ fun AllAppsChart(
     Timber.d("Drawing Acrs - $appSets - total: $total, sweeps: $sweepAngles")
     Box(contentAlignment = Alignment.Center) {
         Canvas(
-            modifier = Modifier.align(Alignment.Center)
+            modifier = Modifier
+                .align(Alignment.Center)
                 .size(size)
         ) {
             if (total > 0) {
-                var startAngle = 90f + bottomGap / 2
+                val startAngle = 90f + bottomGap / 2
                 val arcRadius = size.toPx() - thickness.toPx()
+
+                var currentAngle = startAngle
+                for (setIndex in appSets.indices) {
+                    Timber.d("drawing arc from $startAngle for ${sweepAngles[setIndex]}")
+                    // val brush = BlueGradientBrush
+                    drawArc(
+                        color = appSets[setIndex].second,
+                        startAngle = currentAngle,
+                        sweepAngle = sweepAngles[setIndex],
+                        useCenter = false,
+                        style = Stroke(width = thickness.toPx(), cap = StrokeCap.Round),
+                        size = Size(arcRadius, arcRadius),
+                        topLeft = Offset(
+                            x = (size.toPx() - arcRadius) / 2,
+                            y = (size.toPx() - arcRadius) / 2
+                        ),
+                        //blendMode = BlendMode.Multiply,
+                        alpha = 1f,
+                    )
+                    currentAngle += sweepAngles[setIndex]
+                }
 
                 // underlaying arc for colorblending
                 drawArc(
@@ -57,29 +80,9 @@ fun AllAppsChart(
                         x = (size.toPx() - arcRadius) / 2,
                         y = (size.toPx() - arcRadius) / 2
                     ),
-                    //blendMode = BlendMode.Multiply,
+                    blendMode = BlendMode.Softlight,
                     alpha = 1f
                 )
-
-                for (setIndex in appSets.indices) {
-                    Timber.d("drawing arc from $startAngle for ${sweepAngles[setIndex]}")
-                    // val brush = BlueGradientBrush
-                    drawArc(
-                        color = appSets[setIndex].second,
-                        startAngle = startAngle,
-                        sweepAngle = sweepAngles[setIndex],
-                        useCenter = false,
-                        style = Stroke(width = thickness.toPx(), cap = StrokeCap.Round),
-                        size = Size(arcRadius, arcRadius),
-                        topLeft = Offset(
-                            x = (size.toPx() - arcRadius) / 2,
-                            y = (size.toPx() - arcRadius) / 2
-                        ),
-                        //blendMode = BlendMode.Multiply,
-                        alpha = 1f,
-                    )
-                    startAngle += sweepAngles[setIndex]
-                }
             } else {
                 val arcRadius = size.toPx() - thickness.toPx()
                 val brush = BlueGradientBrush
@@ -94,7 +97,7 @@ fun AllAppsChart(
                         x = (size.toPx() - arcRadius) / 2,
                         y = (size.toPx() - arcRadius) / 2
                     ),
-                    alpha = 0.9f,
+                    alpha = 1f,
                     blendMode = BlendMode.Multiply
                 )
             }
@@ -102,6 +105,10 @@ fun AllAppsChart(
     }
 }
 
-fun mapToArc(size: Int, maxSize: Int, arcMaxSize: Float): Float {
-    return (size / maxSize) * arcMaxSize
+@Preview
+@Composable
+fun AllAppsChartPreview(){
+    val sets = listOf(4, 4, 4)
+    val colors = listOf(unacceptableScoreColor, questionableScoreColor, acceptableScoreColor)
+    AllAppsChart(appSets = sets.zip(colors))
 }
